f0d4cac21a0a1ce8989ea4f49efd5df7346ba10d
Shaamala <shaamalajabbar@gmail.com>
2018-01-23 14:19:45 -0500

    first commit of gitPractice

---
+++ src/NewGitDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println(" Please enter user first name ");
+		
+		String name =scan.nextLine();
+		
+		System.out.println(" User name is : " + name);
+		
+	}
+
+}
